8c65971a01f519ae75f45ed49f829c91961f5a6e
Souvik Basu <souvikbasu@gmail.com>
2018-09-06 22:30:16 +0530

    resolve react in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,39 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index.js'
+    filename: 'index.js',      
+    library: 'reactContainerAsProp',      
+    libraryTarget: 'umd',      
+    publicPath: '/dist/',      
+    umdNamedDefine: true  
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  resolve: {      
+    alias: {          
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
+    }  
+  },  
+  externals: {      
+    // Don't bundle react or react-dom      
+    react: {          
+        commonjs: "react",          
+        commonjs2: "react",          
+        amd: "React",          
+        root: "React"      
+    },      
+    "react-dom": {          
+        commonjs: "react-dom",          
+        commonjs2: "react-dom",          
+        amd: "ReactDOM",          
+        root: "ReactDOM"      
+    }  
+} 
 }
 
 
